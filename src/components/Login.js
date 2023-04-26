cf35978564c04743af2a04cd953fab277f2e0c74
takyafumin <takyafumin@gmail.com>
2023-04-26 17:07:16 +0900

    ログイン機能実装

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,23 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react'
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+    const navigate = useNavigate();
+    const loginInWithGoogle = () => {
+        // Googleでログイン
+        signInWithPopup(auth, provider).then((result) => {
+            localStorage.setItem("isAuth", "true")
+            setIsAuth(true)
+            navigate("/")
+        });
+    }
     return (
-        <div>Login</div>
+        <div>
+            <p>ログインして始める</p>
+            <button onClick={loginInWithGoogle}>Googleでログイン</button>
+        </div>
     )
 }
 
