da6c7a3ffcf4bbc475639dc3077b9f34a56c4530
takyafumin <takyafumin@gmail.com>
2023-05-05 21:10:10 +0900

    投稿表示画面作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='homePage'>
+            <div className="postContents">
+                <div className="postHeader">
+                    <h1>タイトル</h1>
+                </div>
+                <div className="postTextContainer">
+                    今はReactの学習中です。aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                </div>
+                <div className="nameAndDeleteButton">
+                    <h3>@takyafumin</h3>
+                    <button>削除</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
