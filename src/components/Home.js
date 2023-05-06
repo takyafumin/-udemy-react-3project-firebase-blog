a396c750e546556db6363b60befb183f7cad61f8
takyafumin <takyafumin@gmail.com>
2023-05-06 19:47:21 +0900

    自分の投稿だけを削除できるよう修正

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Home.css'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from '../firebase'
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
+import { auth, db } from '../firebase'
 
 const Home = () => {
     const [postList, setPostList] = useState([])
@@ -13,6 +13,11 @@ const Home = () => {
         }
         getPosts();
     }, [])
+
+    const handleDelete = async (id) => {
+        await deleteDoc(doc(db, "posts", id));
+        window.location.href = '/';
+    }
 
     return (
         <div className='homePage'>
@@ -27,7 +32,9 @@ const Home = () => {
                         </div>
                         <div className="nameAndDeleteButton">
                             <h3>@{post.author.username}</h3>
-                            <button>削除</button>
+                            {post.author.id === auth.currentUser?.uid && (
+                                <button onClick={() => handleDelete(post.id)}>削除</button>
+                            )}
                         </div>
                     </div>
                 );
