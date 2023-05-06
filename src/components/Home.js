ea6ae57a25edea6ad65c3035b4c87050358bd67e
takyafumin <takyafumin@gmail.com>
2023-05-06 17:35:57 +0900

    投稿の表示処理実装

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const Home = () => {
+    const [postList, setPostList] = useState([])
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(collection(db, "posts"));
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        }
+        getPosts();
+    }, [])
+
     return (
         <div className='homePage'>
-            <div className="postContents">
-                <div className="postHeader">
-                    <h1>タイトル</h1>
-                </div>
-                <div className="postTextContainer">
-                    今はReactの学習中です。aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                </div>
-                <div className="nameAndDeleteButton">
-                    <h3>@takyafumin</h3>
-                    <button>削除</button>
-                </div>
-            </div>
+            {postList.map((post) => {
+                return (
+                    <div className="postContents" key={post.id}>
+                        <div className="postHeader">
+                            <h1>{post.title}</h1>
+                        </div>
+                        <div className="postTextContainer">
+                            {post.postText}
+                        </div>
+                        <div className="nameAndDeleteButton">
+                            <h3>@{post.author.username}</h3>
+                            <button>削除</button>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     )
 }
